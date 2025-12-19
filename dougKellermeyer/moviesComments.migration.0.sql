3050f698ba02161da817b7dd1e4e22f82c585d21
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2025-12-18 22:54:44 -0500

    added migration SQL

--- dougKellermeyer/moviesComments.migration.0.sql
+++ dougKellermeyer/moviesComments.migration.0.sql
@@ -13,10 +13,26 @@
 --
 --    SELECT $id, $other$value, j.joined_value FROM my_join_table WHERE id = $id;
 
-CREATE TABLE movies_comments (
-    -- A common ID that's joined over.
-    id           INTEGER PRIMARY KEY NOT NULL,
-    -- A value that's updated by one lambda, and read by another.
-    joined_value TEXT NOT NULL
+-- Example structure only; adjust columns to match your actual schemas.
+
+CREATE TABLE movies (
+  movie_id   TEXT PRIMARY KEY NOT NULL,
+  title      TEXT NOT NULL,
+  year       INTEGER,
+  updated_at TEXT NOT NULL,
+  plot       TEXT NOT NULL,
+  num_mflix_comments    INTEGER
 );
+
+CREATE TABLE comments (
+  comment_id TEXT PRIMARY KEY NOT NULL,
+  movie_id   TEXT NOT NULL,
+  name       TEXT,
+  email      TEXT,
+  text       TEXT,
+  date TEXT NOT NULL,
+  FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
+);
+
+CREATE INDEX idx_comments_movie_id ON comments (movie_id);
 
